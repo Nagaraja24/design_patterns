d94a48c99f87f1df4ecca2a204fb8361bfeb9d3d
Nagaraja <nagaraja.rbs@gmail.com>
2018-12-30 18:08:20 +0100

    singleton pattern

---
+++ design-patterns/src/com/dp/singleton/Singleton.java
@@ -0,0 +1,27 @@
+package com.dp.singleton;
+
+//Double Checked Locking based Java implementation of 
+//singleton design pattern 
+public class Singleton {
+
+	private volatile static Singleton instance;
+
+	private Singleton() {
+
+	}
+
+	public static Singleton getInstance() {
+
+		if (instance == null) {
+			// To make thread safe
+			synchronized (Singleton.class) {
+				if (instance == null) {
+					instance = new Singleton();
+				}
+			}
+
+		}
+
+		return instance;
+	}
+}
